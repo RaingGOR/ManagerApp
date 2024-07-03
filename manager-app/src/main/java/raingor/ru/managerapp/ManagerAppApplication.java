fab169e5325eeea4f36c645805c8ba01603e215d
Raingor <kota-2019@inbox.ru>
2024-07-03 17:12:28 +0300

    Start project with microservices

---
+++ manager-app/src/main/java/raingor/ru/managerapp/ManagerAppApplication.java
@@ -0,0 +1,13 @@
+package raingor.ru.managerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerAppApplication.class, args);
+    }
+
+}
---
+++ manager-app/src/test/java/raingor/ru/managerapp/ManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package raingor.ru.managerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
