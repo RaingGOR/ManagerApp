4310f11c393a35b03ad43d53d4f16d3f949380d6
Raingor <kota-2019@inbox.ru>
2024-07-07 12:55:22 +0300

    * add base SPRING SECURITY

---
+++ catalogue-service/src/main/java/raingor/ru/catalogueservice/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package raingor.ru.catalogueservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests( auth ->
+                        auth.requestMatchers("/api/**").hasRole("SERVICE"))
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
--- manager-app/src/main/java/raingor/ru/managerapp/config/ClientBeans.java
+++ manager-app/src/main/java/raingor/ru/managerapp/config/ClientBeans.java
@@ -3,18 +3,23 @@ package raingor.ru.managerapp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import raingor.ru.managerapp.client.ProductsRestClientImpl;
 
 @Configuration
 public class ClientBeans {
     @Bean
-    ProductsRestClientImpl restClient(@Value("${raingor.services.catalogue.uri:http://localhost:8081}")
-                                      String catalogueBaseUrl) {
+    ProductsRestClientImpl restClient(
+            @Value("${services.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl,
+            @Value("${services.catalogue.username:catalogue_service_user}") String username,
+            @Value("${services.catalogue.password:password}") String password
+                                      ) {
 
         return new ProductsRestClientImpl(RestClient
                 .builder()
                 .baseUrl(catalogueBaseUrl)
+                .requestInterceptor(new BasicAuthenticationInterceptor(username,password))
                 .build()
         );
     }
